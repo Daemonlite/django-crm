95fe4c631c6bad37d225259cf377d1248b505859
Daemonlite <khemikal2016@gmail.com>
2023-07-12 07:07:30 -0700

    ali

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
@@ -47,3 +48,15 @@ class Order(models.Model):
 
     def __str__(self):
         return self.status
+    
+
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.SET_NULL,blank=True,null=True)
+    first_name = models.CharField(max_length=200,null=True)
+    last_name = models.CharField(max_length=200,null=True)
+    phone = models.CharField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.first_name
+
+

dea372f7c2178a94df77686f28744a90730eb531
Eugene <khemikal2016@gmail.com>
2023-10-24 11:02:12 +0000

    updated admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(Tags)
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'email', 'date_created']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category','description' ,'date_created']
+
+@admin.register(Tags)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'product', 'date_created', 'status']
+
+
+
+
